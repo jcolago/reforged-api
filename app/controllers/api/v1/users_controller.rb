76a7b36465f7b9a510f59c31098c7de5d603ba01
jcolago <jcolago2005@gmail.com>
2024-08-28 15:28:30 -0500

    Updated user controller and model

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,56 @@
+module Api
+  module V1
+    class UsersController < ApplicationController
+      def index
+        @users = User.all
+        render json: @users.as_json(except: :password_digest)
+      end
+
+      def show
+        @user = User.find(params[:id])
+        render json: @user.as_json(except: :password_digest)
+      end
+
+      def create
+        @user = User.new(user_params)
+        if @user.save
+          render json: @user.as_json(except: :password_digest), status: :created
+        else
+          render json: @user.errors, status: :unprocessable_entity
+        end
+      end
+
+      def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+          render json: @user.as_json(except: :password_digest)
+        else
+          render json: @user.errors, status: :unprocessable_entity
+        end
+      end
+
+      def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        head :no_content
+      end
+      
+      private
+
+      def user_params
+        params.require(:user).permit(:email, :password, :password_confirmation)
+      end
+
+      # Need to revisit this, will check in when front end is set up
+      def login
+        @user = User.find_by(email: params[:email])
+        if @user&.authenticate(params[:password])
+          # Here you would generate and return a token for authentication
+          render json: { message: 'Logged in successfully', user_id: @user.id }
+        else
+          render json: { error: 'Invalid email or password' }, status: :unauthorized
+        end
+      end
+    end
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }, if: :password_digest_changed?
 end
