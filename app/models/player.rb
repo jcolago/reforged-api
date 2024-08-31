aedb59375652efff649481195c9886696fc6b100
jcolago <jcolago2005@gmail.com>
2024-08-30 21:35:11 -0500

    Updated player model

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,34 @@
 class Player < ApplicationRecord
+  belongs_to :game
+
+  validates :name, :character, presence: true
+  validates :level, :current_hp, :total_hp, :armor_class, :speed, :initiative_bonus, :strength, :str_save, :dexterity, :dex_save, :constitution, :con_save, :intelligence, :int_save, :wisdom, :wis_save,  :charisma, :cha_save, presence: true, numericality: { only_integer: true }
+  validates :displayed, inclusion: { in: [ true, false ] }
+  validates :game_id, presence: true
+
+  validate :current_hp_not_exceeding_total_hp
+
+  before_save :calculate_ability_bonuses
+
+  ABILITY_SCORES = [ :strength, :dexterity, :constitution, :intelligence, :wisdom, :charisma ]
+
+  private
+
+  def current_hp_not_exceeding_total_hp
+    if current_hp > total_hp
+      errors.add(:current_hp, "can't exceed total HP")
+    end
+  end
+
+  def calculate_ability_bonuses
+    ABILITY_SCORES.each do |ability|
+      score = send(ability)
+      bonus = calculate_bonus(score)
+      send("#{ability.to_s[0..2]}_bonus=", bonus)
+    end
+  end
+
+  def calculate_bonus(score)
+    (score - 10) / 2
+  end
 end
