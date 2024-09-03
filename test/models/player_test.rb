9162e80903f67ce7eb82624202d93a3f5b0cc82c
jcolago <jcolago2005@gmail.com>
2024-09-03 12:13:59 -0500

    Updated player model tests

--- test/models/player_test.rb
+++ test/models/player_test.rb
@@ -21,4 +21,36 @@ class PlayerTest < ActiveSupport::TestCase
 
     assert_equal -5, player.str_bonus
   end
+
+  test "calling calculate_ability_bonuses for dexterity 20" do
+    player = players(:joe)
+    player.dexterity = 20
+    player.save
+
+    assert_equal 5, player.dex_bonus
+  end
+
+  test "calling calculate_ability_bonuses for dexterity 14" do
+    player = players(:joe)
+    player.dexterity = 6
+    player.save
+
+    assert_equal -2, player.dex_bonus
+  end
+
+  test "calling calculate_ability_bonuses for constitution 20" do
+    player = players(:joe)
+    player.constitution = 20
+    player.save
+
+    assert_equal 5, player.con_bonus
+  end
+
+  test "calling calculate_ability_bonuses for constitution 8" do
+    player = players(:joe)
+    player.constitution = 8
+    player.save
+
+    assert_equal -1, player.con_bonus
+  end
 end
