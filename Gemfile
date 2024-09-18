ef74a387a9eeb32be20ba017bac3fb62e2bd282a
jcolago <jcolago2005@gmail.com>
2024-09-18 15:31:58 -0500

    Added models for current sessions, updated user model

--- Gemfile
+++ Gemfile
@@ -24,7 +24,7 @@ gem "bcrypt", "~> 3.1.7"
 # Use JWT for user authentication [https://jwt.io/]
 gem "jwt"
 
-gem 'rack-cors'
+gem "rack-cors"
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem "tzinfo-data", platforms: %i[ windows jruby ]
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :games, foreign_key: "dm_id"
+  has_many :sessions, dependent: :destroy
+  normalizes :email_address, with: ->(e) { e.strip.downcase }
 
   has_secure_password
   validates :email, presence: true, uniqueness: true
