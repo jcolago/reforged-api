42991b0c31f04fdbd9a44a63b798a43368aaaab2
Joseph Colago <jcolago2005@gmail.com>
2024-11-11 19:28:02 -0600

    Updated routes and application for use with front end

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
-  include Authentication
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  protect_from_forgery with: :null_session
+  
+  def index
+    render file: Rails.root.join('public', 'index.html')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       post :login, to: "sessions#login"
       get :me, to: "sessions#me"
       delete :logout, to: "sessions#logout"
+      
       resources :player_conditions
       resources :conditions
       resources :players do
@@ -20,20 +21,6 @@ Rails.application.routes.draw do
       end
       resources :games
       resources :users
-      end
-
-      # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-      # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-      # Can be used by load balancers and uptime monitors to verify that the app is live.
-      get "up" => "rails/health#show", as: :rails_health_check
-
-      # Render dynamic PWA files from app/views/pwa/*
-      get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-      get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
-      # Defines the root path route ("/")
-      root "pages#home"
-      post "login", to: "users#login"
     end
   end
+end
